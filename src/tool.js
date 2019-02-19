3c726d5fed39e9f0429c5c4d7356d3cc17b4041e
Gagan <g.deepsingh1@gmail.com>
2019-02-19 06:52:35 +0530

    async test

--- src/tool.js
+++ src/tool.js
@@ -1,5 +1,20 @@
+let https = require('https');
+
 module.exports = {
     printName(person) {
         return `${person.last}, ${person.first}`;
+    },
+    load(person, cb) {
+        https.get(`https://en.wikipedia.org/wiki/${person.first}_${person.last}`, function(result) {
+            let p = '';
+
+            result.on('data', (val) => {
+                p += val;
+            });
+
+            result.on('end', () => {
+                cb(p);
+            });
+        });
     }
 }
--- test/tool.spec.js
+++ test/tool.spec.js
@@ -6,4 +6,13 @@ describe('tool test', function() {
         let value = tool.printName({first: 'first', last: 'last'});
         chai.expect(value).to.be.equal('last, first');
     });
+
+    describe('aynchronous test', function() {
+        it('load page', function(done) {
+            tool.load({first: 'Abraham', last: 'Lincoln'}, function(html) {
+                chai.expect(html).to.be.ok;
+                done();
+            });
+        });
+    });
 });
