0574778500ac175a8b8277ee3ab7a0397593d635
Gagan <g.deepsingh1@gmail.com>
2019-02-19 08:32:09 +0530

    sinon stubs

--- test/order.spec.js
+++ test/order.spec.js
@@ -14,8 +14,12 @@ describe('order', function() {
         this.console = {
             log: sinon.spy()
         };
+        this.warehouse = {
+            packageAndShip: sinon.stub().yields(123456789)
+        };
         order.__set__('inventoryData', testdata);
         order.__set__('console', this.console);
+        order.__set__('warehouse', this.warehouse);
     });
     
     it('order a product', function(done) {
@@ -23,6 +27,22 @@ describe('order', function() {
         order.orderItem('P', 9, function() {
             chai.expect(_this.console.log.callCount).to.be.equal(2);
             done();
+        });
+    });
+
+    describe('warehouse', function() {
+        beforeEach(function() {
+            this.callback = sinon.spy();
+            order.orderItem('P', 9, this.callback);
+        });
+
+        it('correct number has been passed to it', function(done) {
+            chai.expect(this.callback.calledWith(123456789)).to.equal(true);  
+            done();          
+        });
+
+        it('call with correct arguments', function() {
+            chai.expect(this.warehouse.packageAndShip.calledWith('P', 9)).to.equal(true);
         });
     });
 });
