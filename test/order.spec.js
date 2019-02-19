32b489a531e0b0e9a149db371e5b81f63812eea3
Gagan <g.deepsingh1@gmail.com>
2019-02-19 08:07:55 +0530

    sinon spy

--- test/order.spec.js
+++ test/order.spec.js
@@ -1,6 +1,7 @@
-let chai = require('chai');
-let rewire = require('rewire');
-let order = rewire('../lib/order');
+const chai = require('chai');
+const rewire = require('rewire');
+const order = rewire('../lib/order');
+const sinon = require('sinon');
 
 describe('order', function() {
     beforeEach(function() {
@@ -10,11 +11,17 @@ describe('order', function() {
                 qty: 9
             }
         ];
+        this.console = {
+            log: sinon.spy()
+        };
         order.__set__('inventoryData', testdata);
+        order.__set__('console', this.console);
     });
     
     it('order a product', function(done) {
+        let _this = this;
         order.orderItem('P', 9, function() {
+            chai.expect(_this.console.log.callCount).to.be.equal(2);
             done();
         });
     });
